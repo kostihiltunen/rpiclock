34fb21bef0b9b41842fc451062485324c235100f
Kosti Hiltunen <kosti.hiltunen@gmail.com>
2022-10-24 11:05:27 +0300

    countdown timer testing2

--- html/index.js
+++ html/index.js
@@ -12,6 +12,32 @@ socket.on("buttonA", (pressed, data) => {
   elBegin.style.display = "none";
   elSuccess.style.display = "none";
   elGameOn.style.display = "inline";
+
+  const endTime = new Date().getTime() + 1000 * 60 * 60;
+
+  const timer = () => {
+    let now = new Date().getTime();
+    t = endTime - now;
+
+    if (t >= 0) {
+      let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+      let secs = Math.floor((t % (1000 * 60)) / 1000);
+
+      document.getElementById("timer-hours").innerHTML =
+        ("0" + hours).slice(-2) + "<span class='label'>HR(S)</span>";
+
+      document.getElementById("timer-mins").innerHTML =
+        ("0" + mins).slice(-2) + "<span class='label'>MIN(S)</span>";
+
+      document.getElementById("timer-secs").innerHTML =
+        ("0" + secs).slice(-2) + "<span class='label'>SEC(S)</span>";
+    } else {
+      elGameOn.style.display = "none";
+      elGameOver.style.display = "inline";
+    }
+  };
+
   setInterval(timer, 1000);
 });
 
@@ -19,29 +45,5 @@ socket.on("buttonB", (pressed, data) => {
   elGameOn.style.display = "none";
   elSuccess.style.display = "inline";
   solveTime = t;
+  console.log(solveTime); //for testing purpose
 });
-
-const endTime = new Date().getTime() + 1000 * 60 * 60;
-
-const timer = () => {
-  let now = new Date().getTime();
-  t = endTime - now;
-
-  if (t >= 0) {
-    let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-    let secs = Math.floor((t % (1000 * 60)) / 1000);
-
-    document.getElementById("timer-hours").innerHTML =
-      ("0" + hours).slice(-2) + "<span class='label'>HR(S)</span>";
-
-    document.getElementById("timer-mins").innerHTML =
-      ("0" + mins).slice(-2) + "<span class='label'>MIN(S)</span>";
-
-    document.getElementById("timer-secs").innerHTML =
-      ("0" + secs).slice(-2) + "<span class='label'>SEC(S)</span>";
-  } else {
-    elGameOn.style.display = "none";
-    elGameOver.style.display = "inline";
-  }
-};
